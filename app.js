c8a4704a95dac4f82f082451b12c17f28e4bcfec
Erin Cooper <e.m.cooper96@gmail.com>
2017-07-21 20:36:27 -0400

    First update in FOREVER

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+
+var browserSync = require('browser-sync').create('myServer');
+browserSync.init({
+  server: "./",
+  notify: false
+});
+
+var currentSession = require('browser-sync').get('myServer');
+currentSession.watch('*.html').on('change', currentSession.reload);
+console.log("Watching HTML files...");
+currentSession.watch('assets/css/custom.css').on('change', currentSession.reload);
+console.log("Watching custom.css...");
